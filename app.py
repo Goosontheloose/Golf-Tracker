fc53b375ef0a1300bacd8949cb08f4f15ab14c21
Goosontheloose <frederikswanepoel1@gmail.com>
2026-07-19 19:00:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ def get_registry_data():
 API_KEY = st.secrets.get("api_key", "")
 YEAR, TOURN_ID = "2026", "100"
 
-@st.cache_data(ttl=300)
+@st.cache_data(ttl=60)
 def get_live_scores():
     try:
         url = "https://live-golf-data.p.rapidapi.com/leaderboard"
